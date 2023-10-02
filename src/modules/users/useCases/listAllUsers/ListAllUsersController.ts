61d3af45be9ac218b465ba9817b7ab9d8fa9f12e
matheushfp <mathzhf@gmail.com>
2023-10-02 10:43:18 -0300

    complete challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      return response.json(this.listAllUsersUseCase.execute({ user_id }));
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (user.admin === false) {
+      throw new Error("User doesn't have admin privileges");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
